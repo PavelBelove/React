bec7dad7340b1ce95fe8176b22450a5348ede0da
Pavel_Belove <agink@inbox.ru>
2019-03-28 21:24:38 +0300

    class Developer

---
+++ src/Developer.js
@@ -0,0 +1,10 @@
+export class Developer {
+    constructor(name) {
+        this.name = name;
+    }
+
+    infoPrompt(){
+        prompt("class from Developer.js")
+    }
+}
+
--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import App from './App';
+import Developer from './Developer.js'
 
 ReactDom.render(
   <App />,
   // указываем куда будет вставляться React приложение на странице
   document.querySelector('#web-page')
 );
+
+let dev = new Developer("Vasya");
+dev.infoPrompt();
